b8a3621317ab9c0737f10dd5d20eca84bb9c8b19
tukaram-07 <tuka.gaule@gmail.com>
2024-11-08 20:11:49 +0530

    adding features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const {ApolloServer} = require('@apollo/server');
+const { typeDefs } = require('./schema')
+const { resolvers } = require('./resolvers')
 const {expressMiddleware} = require('@apollo/server/express4');
 const bosyParser = require('body-parser');
 const cors = require('cors');
@@ -7,27 +9,7 @@ const { query } = require('express');
 
 async function startServer() {
     const app =express();
-    const server = new ApolloServer({
-        typeDefs: `
-        type Book {
-        id: ID!
-        title: String!
-        }
-        
-        type Query {
-        books: [Book]
-        }
-        `,
-        
-
-        resolvers: {
-            Query: {
-                books: () => [
-                    {id: 1, title: "Anything"},
-                ],
-            }
-        }
-    });
+    const server = new ApolloServer({ resolvers, typeDefs });
 
     app.use(bosyParser.json());
     app.use(cors());
